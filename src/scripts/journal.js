eb9470dbe01b511d73fff993d513513529d83ee1
Cassie Boyd <cassandra.bacon@gmail.com>
2019-10-29 13:34:22 -0500

    Using querySelector

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -8,6 +8,21 @@
 
 import API from "./data.js"
 import renderDom from "./entriesDOM.js"
+import entryManager from "./entryComponent.js"
 
 API.getJournalEntries()
 .then(response => renderDom.renderJournalEntries(response))
+
+const attachEventListenerToEntryButton = () => {
+
+    // DOM is searched for first button tag and assigns it to variable "entryButton"
+const entryButton = document.querySelector("button")
+const concept = document.querySelector("concept-input")
+const entry = document.querySelector("entry-input")
+const mood = document.querySelector("mood-input")
+const dateField = document.querySelector("date-input")
+
+
+// Listens for a click on the button tag.
+entryButton.addEventListener("click", entryManager.makeJournalEntryComponent)
+}
