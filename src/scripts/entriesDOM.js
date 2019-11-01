cf9aaa5ef679daa575f87635e8f921a40c230aa3
Cassie Boyd <cassandra.bacon@gmail.com>
2019-10-31 20:25:20 -0500

    Adding comments

--- src/scripts/entriesDOM.js
+++ src/scripts/entriesDOM.js
@@ -1,14 +1,11 @@
-/*
-    Purpose: To render all journal entries to the DOM
-
-    Arguments: entries (array of objects)
-*/
 
 // Import statement so entryManager can be accessed
 import entryManager from "./entryComponent.js"
 
-
+// renderDom module for rendering journal entries to the DOM
 const renderDom = {
+
+  // Called on main.js and on api.js. Takes data from the API as a parameter.
   renderJournalEntries(entries) {
     let HtmlForAllEntries = ""
     entries.forEach(entry => {
@@ -18,7 +15,7 @@ const renderDom = {
     const logArticle = document.querySelector(".entryLog")
     logArticle.innerHTML = HtmlForAllEntries
   },
-  // Factory function for returning an object. 
+  // Factory function for returning an object. Targets the value of the input fields and assigns them to variables. The variables are then returned in an object.
   buildEntry () {
     const concept = document.querySelector("#concept-input").value
     const entry = document.querySelector("#entry-input").value
